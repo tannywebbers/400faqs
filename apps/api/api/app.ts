394b4657e1641af3aacf99f19aba895e6cdccdd2
tannywebbers <tannywebbers@gmail.com>
2026-09-01 10:43:39 +0100

    ROUTED API FOR VERCEL EXPRESS APP

---
+++ apps/api/api/app.ts
@@ -0,0 +1,8 @@
+import type { IncomingMessage, ServerResponse } from "http";
+import { createApp } from "../src/app";
+
+const app = createApp();
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  app(req, res);
+}
